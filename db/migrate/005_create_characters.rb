94eeff9c83348768fd40913a4a661141288da822
Ariel Zhen <zhenpingping@live.com>
2018-07-26 05:01:54 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
-    creata_table :actors do |t|
+    creata_table :characters do |t|
       t.string :first_name
       t.string :last_name
     end
